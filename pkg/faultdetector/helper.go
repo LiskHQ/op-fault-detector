1da818887e7a921507b54c85e9bdefd6b8b58c02
Incede <33103370+Incede@users.noreply.github.com>
2024-01-31 11:49:50 +0100

    Remove unnnecessary calls per feedback

--- pkg/faultdetector/helper.go
+++ pkg/faultdetector/helper.go
@@ -5,39 +5,18 @@ import (
 	"fmt"
 
 	"github.com/LiskHQ/op-fault-detector/pkg/chain"
-	"github.com/LiskHQ/op-fault-detector/pkg/config"
 	"github.com/LiskHQ/op-fault-detector/pkg/encoding"
 	"github.com/LiskHQ/op-fault-detector/pkg/log"
 )
 
 // FindFirstUnfinalizedOutputIndex finds and returns the first L2 output index that has not yet passed the fault proof window.
-func FindFirstUnfinalizedOutputIndex(ctx context.Context, logger log.Logger, fpw uint64, faultDetectorConfig *config.FaultDetectorConfig) (uint64, error) {
-	l2RpcApi, err := chain.GetAPIClient(ctx, faultDetectorConfig.L2RPCEndpoint, logger)
-	if err != nil {
-		logger.Errorf("Failed to create API client for L2 Provider with given endpoint: %s, error: %w", faultDetectorConfig.L2RPCEndpoint, err)
-		return 0, err
-	}
-	l2ChainID, err := l2RpcApi.GetChainID(ctx)
-	if err != nil {
-		logger.Errorf("Failed to get L2 provider's chainID: %d, error: %w", encoding.MustConvertBigIntToUint64(l2ChainID), err)
-		return 0, err
-	}
-	chainConfig := &chain.ConfigOptions{
-		L1RPCEndpoint:                 faultDetectorConfig.L1RPCEndpoint,
-		ChainID:                       encoding.MustConvertBigIntToUint64(l2ChainID),
-		L2OutputOracleContractAddress: faultDetectorConfig.L2OutputOracleContractAddress,
-	}
-	oracleContractAccessor, err := chain.NewOracleAccessor(ctx, chainConfig)
-	if err != nil {
-		logger.Errorf("Failed to create Oracle contract accessor with chainID: %d, L1 endpoint: %s and L2OutputOracleContractAddress: %s, error: %w", encoding.MustConvertBigIntToUint64(l2ChainID), faultDetectorConfig.L1RPCEndpoint, faultDetectorConfig.L2OutputOracleContractAddress, err)
-		return 0, err
-	}
+func FindFirstUnfinalizedOutputIndex(ctx context.Context, logger log.Logger, fpw uint64, oracleAccessor *chain.OracleAccessor, l2RpcApi *chain.ChainAPIClient) (uint64, error) {
 	latestBlockHeader, err := l2RpcApi.GetLatestBlockHeader(ctx)
 	if err != nil {
 		logger.Errorf("Failed to get latest block header from L2 provider, error: %w", err)
 		return 0, err
 	}
-	totalOutputsBigInt, err := oracleContractAccessor.GetNextOutputIndex()
+	totalOutputsBigInt, err := oracleAccessor.GetNextOutputIndex()
 	if err != nil {
 		logger.Errorf("Failed to get next output index, error: %w", err)
 		return 0, err
@@ -50,7 +29,7 @@ func FindFirstUnfinalizedOutputIndex(ctx context.Context, logger log.Logger, fpw
 	for lo != hi {
 		mid := (lo + hi) / 2
 		midBigInt := encoding.MustConvertUint64ToBigInt(mid)
-		outputData, err := oracleContractAccessor.GetL2Output(midBigInt)
+		outputData, err := oracleAccessor.GetL2Output(midBigInt)
 		if err != nil {
 			logger.Errorf("Failed to get L2 output for index: %d, error: %w", midBigInt, err)
 			return 0, err
